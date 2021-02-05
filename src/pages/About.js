5a5e7bbd48a29c753db7430bc7d14881e13bb39c
Shabix <kmharouna@gmail.com>
2021-02-05 17:56:48 +0300

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Animi numquam
+        tenetur vero nihil voluptates maxime hic quaerat deserunt molestiae
+        alias nobis magni asperiores illum blanditiis corrupti, facilis, porro,
+        labore praesentium! Quisquam repudiandae ipsum architecto! Impedit ex
+        nisi alias vitae perferendis?
+      </p>
+    </section>
+  );
+};
+
+export default About;
